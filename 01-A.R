e154449e59f722d6862229f18834be92249e4286
Grigorij Schleifer <chaosambulance@googlemail.com>
2021-04-14 19:35:28 +0200

    still fixing status columns

--- 01-A.R
+++ 01-A.R
@@ -99,13 +99,84 @@ cohort_pao2 <- hyper %>%
                 "90 yr",
                 ">=90 yr"
             )
-    )) %>%  # MERGING admission to hyper
-    select(., !c("X")) %>% 
-    inner_join(admissions.real, by = "hadm_id") # removing "X" column
-    
-# create a dataframe for status updates
-status_cohort <- cohort_pao2 %>% 
-    select(marital_status, religion, ethnicity, insurance)
+    )) %>%
+    # removing "X" column
+    select(., !c("X")) %>%
+    # MERGING admission to hyper
+    inner_join(admissions.real, by = "hadm_id") %>% 
+    select(marital_status, religion, ethnicity, insurance) %>%
+    mutate(
+        marital_status = case_when(
+            marital_status == "SINGLE" ~ "SINGLE",
+            marital_status == "MARRIED" ~ "MARRIED",
+            marital_status == "SEPARATED" ~ "SEPARATED",
+            marital_status == "DIVORCED" ~ "DIVORCED",
+            marital_status == "WIDOWED" ~ "WIDOWED",
+            marital_status == "UNKNOWN (DEFAULT)" ~ "UNKNOWN (DEFAULT)",
+            marital_status == "" ~ "UNKNOWN (DEFAULT)"
+        )
+    ) %>%
+    mutate(
+        # assign religion categories
+        religion = case_when(
+            religion == "UNOBTAINABLE" ~ "UNKNOWN (DEFAULT)",
+            religion == "NOT SPECIFIED" ~ "NOT SPECIFIED",
+            religion == "" ~ "UNKNOWN (DEFAULT)",
+            ###
+            religion == "7TH DAY ADVENTIST" ~ "CHRISTIAN",
+            religion == "BAPTIST" ~ "CHRISTIAN",
+            religion == "CHRISTIAN SCIENTIST" ~ "CHRISTIAN",
+            religion == "PROTESTANT QUAKER" ~ "CHRISTIAN",
+            religion == "JEHOVAH'S WITNESS" ~ "CHRISTIAN",
+            religion == "CATHOLIC" ~ "CHRISTIAN",
+            religion == "GREEK ORTHODOX" ~ "CHRISTIAN",
+            religion == "ROMANIAN EAST. ORTH" ~ "CHRISTIAN",
+            religion == "EPISCOPALIAN" ~ "CHRISTIAN",
+            ###
+            religion == "OTHER" ~ "OTHER",
+            religion == "BUDDHIST" ~ "OTHER",
+            religion == "MUSLIM" ~ "OTHER",
+            religion == "UNITARIAN-UNIVERSALIST" ~ "OTHER",
+            religion == "JEWISH" ~ "OTHER",
+            religion == "HEBREW" ~ "OTHER"
+        )
+    ) %>%
+    mutate(
+        ethnicity = case_when(
+            ethnicity == "ASIAN" ~ "ASIAN",
+            ethnicity == "ASIAN - ASIAN INDIAN" ~ "ASIAN",
+            ethnicity == "ASIAN - CHINESE" ~ "ASIAN",
+            ###
+            ethnicity == "BLACK/AFRICAN" ~ "BLACK",
+            ethnicity == "BLACK/AFRICAN AMERICAN" ~ "BLACK",
+            ethnicity == "BLACK/CAPE VERDEAN" ~ "BLACK",
+            ethnicity == "BLACK/HAITIAN" ~ "BLACK",
+            ###
+            ethnicity == "HISPANIC OR LATINO" ~ "HISPANIC",
+            ethnicity == "HISPANIC/LATINO - DOMINICAN" ~ "HISPANIC",
+            ethnicity == "HISPANIC/LATINO - GUATEMALAN" ~ "HISPANIC",
+            ethnicity == "HISPANIC/LATINO - PUERTO RICAN" ~ "HISPANIC",
+            ###
+            ethnicity == "WHITE" ~ "WHITE",
+            ethnicity == "WHITE - BRAZILIAN" ~ "WHITE",
+            ethnicity == "WHITE - RUSSIAN"  ~ "WHITE",
+            ###
+            ethnicity == "OTHER" ~ "OTHER",
+            ethnicity == "MULTI RACE ETHNICITY" ~ "OTHER",
+            ethnicity == "PATIENT DECLINED TO ANSWER" ~ "OTHER",
+            ethnicity == "UNABLE TO OBTAIN" ~ "OTHER",
+            ethnicity == "UNKNOWN/NOT SPECIFIED" ~ "OTHER"
+        )
+    ) %>%
+    mutate(
+        insurance = case_when(
+            insurance == "Government" ~ "Private/goverment/self",
+            insurance == "Self Pay" ~ "Private/goverment/self",
+            insurance == "Private" ~ "Private/goverment/self",
+            insurance == "Medicaid" ~ "Medicaid/Medicare",
+            insurance == "Medicare" ~ "Medicaid/Medicare"
+        )
+    )
 
 # change all factors inside the columns to characters
 status_cohort[] <- lapply(status_cohort, as.character)
--- try_and_error.R
+++ try_and_error.R
@@ -1,14 +1,22 @@
-cohort_pao2_status_short_test <- cohort_pao2 %>%
-    select(marital_status, religion, ethnicity, insurance) %>%
+status_cohort <- status_cohort_original %>%
+    # select(marital_status, religion, ethnicity, insurance) %>%
     mutate(
         marital_status = case_when(
+            marital_status == "SINGLE" ~ "SINGLE",
+            marital_status == "MARRIED" ~ "MARRIED",
+            marital_status == "SEPARATED" ~ "SEPARATED",
+            marital_status == "DIVORCED" ~ "DIVORCED",
+            marital_status == "WIDOWED" ~ "WIDOWED",
+            marital_status == "UNKNOWN (DEFAULT)" ~ "UNKNOWN (DEFAULT)",
             marital_status == "" ~ "UNKNOWN (DEFAULT)"
         )
     ) %>%
-    mutate(
+    mutate( # assign religion categories
         religion = case_when(
             religion == "UNOBTAINABLE" ~ "UNKNOWN (DEFAULT)",
+            religion == "NOT SPECIFIED" ~ "NOT SPECIFIED",
             religion == "" ~ "UNKNOWN (DEFAULT)",
+            ###
             religion == "7TH DAY ADVENTIST" ~ "CHRISTIAN",
             religion == "BAPTIST" ~ "CHRISTIAN",
             religion == "CHRISTIAN SCIENTIST" ~ "CHRISTIAN",
@@ -18,6 +26,8 @@ cohort_pao2_status_short_test <- cohort_pao2 %>%
             religion == "GREEK ORTHODOX" ~ "CHRISTIAN",
             religion == "ROMANIAN EAST. ORTH" ~ "CHRISTIAN",
             religion == "EPISCOPALIAN" ~ "CHRISTIAN",
+            ###
+            religion == "OTHER" ~ "OTHER",
             religion == "BUDDHIST" ~ "OTHER",
             religion == "MUSLIM" ~ "OTHER",
             religion == "UNITARIAN-UNIVERSALIST" ~ "OTHER",
@@ -27,18 +37,25 @@ cohort_pao2_status_short_test <- cohort_pao2 %>%
     ) %>%
     mutate(
         ethnicity = case_when(
+            ethnicity == "ASIAN" ~ "ASIAN",
             ethnicity == "ASIAN - ASIAN INDIAN" ~ "ASIAN",
             ethnicity == "ASIAN - CHINESE" ~ "ASIAN",
+            ###
             ethnicity == "BLACK/AFRICAN" ~ "BLACK",
             ethnicity == "BLACK/AFRICAN AMERICAN" ~ "BLACK",
             ethnicity == "BLACK/CAPE VERDEAN" ~ "BLACK",
             ethnicity == "BLACK/HAITIAN" ~ "BLACK",
-            ethnicity == "HISPANIC OR LATINO" ~ "HISPANIC"
+            ###
+            ethnicity == "HISPANIC OR LATINO" ~ "HISPANIC",
             ethnicity == "HISPANIC/LATINO - DOMINICAN" ~ "HISPANIC",
             ethnicity == "HISPANIC/LATINO - GUATEMALAN" ~ "HISPANIC",
             ethnicity == "HISPANIC/LATINO - PUERTO RICAN" ~ "HISPANIC",
+            ###
+            ethnicity == "WHITE" ~ "WHITE",
             ethnicity == "WHITE - BRAZILIAN" ~ "WHITE",
             ethnicity == "WHITE - RUSSIAN"  ~ "WHITE",
+            ###
+            ethnicity == "OTHER" ~ "OTHER",
             ethnicity == "MULTI RACE ETHNICITY" ~ "OTHER",
             ethnicity == "PATIENT DECLINED TO ANSWER" ~ "OTHER",
             ethnicity == "UNABLE TO OBTAIN" ~ "OTHER",
@@ -49,7 +66,7 @@ cohort_pao2_status_short_test <- cohort_pao2 %>%
         insurance = case_when(
             insurance == "Government" ~ "Private/goverment/self",
             insurance == "Self Pay" ~ "Private/goverment/self",
-            insurance == "Private" ~ "Private/goverment/self"
+            insurance == "Private" ~ "Private/goverment/self",
             insurance == "Medicaid" ~ "Medicaid/Medicare",
             insurance == "Medicare" ~ "Medicaid/Medicare"
         )
